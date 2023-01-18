08385ba458d9445d1974a2d76a8913fb622ac418
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-01-19 00:42:08 +0200

    hw8

--- src/class8/homework/Task_6.java
+++ src/class8/homework/Task_6.java
@@ -1,8 +1,29 @@
 package class8.homework;
 
 public class Task_6 {
+    /*
+    Print the following pattern:
+            *
+
+            *
+
+            *
+            **
+            *
+            **
+            *
+               */
     public static void main(String[] args) {
 
-
+        for (int i = 1; i < 10; i++) {
+            if (i == 2 || i == 4)
+                System.out.println(" ");
+            else if (i == 6 || i == 8) {
+                System.out.println("**");
+            } else
+                System.out.println("*");
+        }
     }
 }
+
+
