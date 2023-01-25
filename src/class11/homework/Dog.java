03f00b5f81f4a71672cf9dd1af63ce3b446a925d
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-01-25 02:02:33 +0200

    12HW

--- src/class11/homework/Dog.java
+++ src/class11/homework/Dog.java
@@ -2,4 +2,18 @@ package class11.homework;
 
 public class Dog {
 
+    String name;
+
+    String breed;
+
+    int age;
+
+    double weight;
+
+
+    void bark() {
+        System.out.println("Woof!");
+    }
+    void howl () { System.out.println("The dog is howling.");
+    }
 }
